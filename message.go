2ddcb1c0d97799f994f9ae553c777b53a9046f8c
rgielen <rgielen@apache.org>
2015-12-12 00:09:57 +0100

    correct typo

--- message.go
+++ message.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Println("This container does nothing at all. It's purpose is to be THERE.")
+	fmt.Println("This container does nothing at all. Its purpose is to be THERE.")
 	fmt.Println("Intended usage: create a data only container")
 	fmt.Println("docker create -v /hostdir/data:/var/data --name my-data-container rgielen/docker-data-minimal")
 }
